cc7d657f3f0a3b49d1a978c1192710bd4d4be6bd
bling-yshs <377178599@qq.com>
2023-10-21 12:59:51 +0800

    3332

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,7 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!!", name)
+    format!("Hello, {}! You've been greeted from Rust!! 哦豁，这里是一闪", name)
 }
 
 fn main() {
